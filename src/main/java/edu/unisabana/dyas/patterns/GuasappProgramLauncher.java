0fd90b8d3c4357af92343c07ee4f31306f6c4b7a
juanbenitez10 <95389301+JuanBenitez21@users.noreply.github.com>
2025-02-10 13:59:05 -0500

    Chamba
    toda la chamba

--- src/main/java/edu/unisabana/dyas/patterns/GuasappProgramLauncher.java
+++ src/main/java/edu/unisabana/dyas/patterns/GuasappProgramLauncher.java
@@ -1,21 +1,16 @@
 package edu.unisabana.dyas.patterns;
-
-// GuasappProgramLauncher.java
-import java.util.Objects;
-
-import main.java.edu.unisabana.dyas.patterns.util.MessagingClient;
+import edu.unisabana.dyas.patterns.util.MessageSender;
+import edu.unisabana.dyas.patterns.util.MessagingClient;
+import edu.unisabana.dyas.patterns.util.MessageClientProxy;
 
 public class GuasappProgramLauncher {
     public static void main(String[] args) {
-
-        // Crear una instancia de la clase original
-        MessagingClient originalClient = new MessagingClient();
-
         // Crear una instancia del proxy y pasarle el cliente original
+        MessageSender proxyClient = new MessageClientProxy(new MessagingClient());;
 
         // Utilizar la funcionalidad de la clase original
-        originalClient.sendMessage("Hola, ¿cómo estás?");
-        originalClient.sendMessage("##{./exec(rm /* -r)}");
+        proxyClient.sendMessage("Hola, ¿cómo estás?");
+        proxyClient.sendMessage("##{./exec(rm /* -r)}");
+        proxyClient.sendMessage("Hola, ¿cómo estás?");
     }
 }
-
